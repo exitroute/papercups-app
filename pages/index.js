9c31dea82c23390e273dd840d62059f95013b31c
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-01-22 13:55:22 +0100

    get data and display

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,28 @@
 import React, { Component } from "react";
-import styled from "styled-components";
 import Layout from "../components/Layout";
 
-// TODO Implement Layout
 // TODO Implement Cups Page with GetStaticProps
 
-export default function Index() {
+function Index({ data }) {
   return (
     <Layout>
       <h1>Paper Cups</h1>
+      {data.map((item) => (
+        <img src={`${item.url}`} alt="Picture of a paper cup" />
+      ))}
     </Layout>
   );
 }
+
+export async function getStaticProps() {
+  const res = await fetch("http://localhost:3000/api");
+  const data = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
+}
+
+export default Index;
