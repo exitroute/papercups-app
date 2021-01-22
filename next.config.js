9e7c9d7973bfa6cca298baad0597481e6a905b5d
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-01-23 00:56:51 +0100

    env variables for deployment to vercel

---
+++ next.config.js
@@ -0,0 +1,19 @@
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      /* development only config options here */
+      env: {
+        API_URL: "http://localhost:3000/api",
+      },
+    };
+  }
+
+  return {
+    /* config options for all phases except development here */
+    env: {
+      API_URL: "https://papercups-app.now.sh/api",
+    },
+  };
+};
