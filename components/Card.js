1062442102a15694079b21b97affee626aa57e9c
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-01-23 00:31:11 +0100

    create Card component with hover method

---
+++ components/Card.js
@@ -0,0 +1,21 @@
+import React, { Component, useState } from "react";
+import styled from "styled-components";
+import { Box } from "./EveryLayout";
+
+const P = styled.p`
+  visibility: ${(props) => (props.inHover ? "visible" : "hidden")};
+`;
+
+export default function Card(item) {
+  const [inHover, setHover] = useState(false);
+
+  return (
+    <Box
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      <img src={`${item.url}`} alt="Picture of a paper cup" />
+      <P inHover={inHover}>{`${item.name}`}</P>
+    </Box>
+  );
+}
