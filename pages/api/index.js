b8a0fd7078880c7e06dd12ffedd15ac834443373
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-01-23 16:27:15 +0100

    change data to mimic json

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,40 +1,42 @@
-const data = [
-  {
-    name: "Paper Cup",
-    price: "25.00",
-    url: "https://picsum.photos/300/200",
-  },
-  {
-    name: "Lost Cup",
-    price: "25.00",
-    url: "https://picsum.photos/300/200",
-  },
-  {
-    name: "Found Cup",
-    price: "25.00",
-    url: "https://picsum.photos/300/200",
-  },
-  {
-    name: "Paper Cup",
-    price: "25.00",
-    url: "https://picsum.photos/300/200",
-  },
-  {
-    name: "Paper Cup",
-    price: "25.00",
-    url: "https://picsum.photos/300/200",
-  },
-  {
-    name: "Paper Cup",
-    price: "25.00",
-    url: "https://picsum.photos/300/200",
-  },
-  {
-    name: "Paper Cup",
-    price: "25.00",
-    url: "https://picsum.photos/300/200",
-  },
-];
+const data = {
+  pictures: [
+    {
+      name: "Paper Cup",
+      price: "25.00",
+      url: "https://picsum.photos/300/200",
+    },
+    {
+      name: "Lost Cup",
+      price: "25.00",
+      url: "https://picsum.photos/300/200",
+    },
+    {
+      name: "Found Cup",
+      price: "25.00",
+      url: "https://picsum.photos/300/200",
+    },
+    {
+      name: "Paper Cup",
+      price: "25.00",
+      url: "https://picsum.photos/300/200",
+    },
+    {
+      name: "Paper Cup",
+      price: "25.00",
+      url: "https://picsum.photos/300/200",
+    },
+    {
+      name: "Paper Cup",
+      price: "25.00",
+      url: "https://picsum.photos/300/200",
+    },
+    {
+      name: "Paper Cup",
+      price: "25.00",
+      url: "https://picsum.photos/300/200",
+    },
+  ],
+};
 
 export default function handler(req, res) {
   res.status(200).json(data);
