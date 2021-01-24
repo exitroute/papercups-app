1d265f49ff763c00e4db15540d2ef117148716a0
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-01-24 23:15:22 +0100

    build out and style modal

--- components/Modal.js
+++ components/Modal.js
@@ -1,8 +1,33 @@
 import React from "react";
 import styled from "styled-components";
 
+const Container = styled.div`
+  background: rgba(0, 0, 0, 0.8);
+  position: relative;
+`;
+
+const Imposter = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  max-width: calc(100% - 2rem);
+  max-height: calc(100% - 2rem);
+`;
+
 const Modal = ({ showModal }) => {
-  return <>{showModal ? <div>Modal</div> : null}</>;
+  return (
+    <>
+      {showModal ? (
+        <Container>
+          <p>Modal</p>
+          <Imposter>
+            <p>Content</p>
+          </Imposter>
+        </Container>
+      ) : null}
+    </>
+  );
 };
 
 export default Modal;
