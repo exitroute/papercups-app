5edcb8513c6029bdb8debfeedfb61aa61a2a6ca7
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-01-24 23:17:44 +0100

    move modal state to pages/index from Card.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,32 @@
 import Layout from "../components/Layout";
 import Card from "../components/Card";
+import Modal from "../components/Modal";
+import React, { useState } from "react";
 
 function Index({ data }) {
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = (data) => {
+    setShowModal((prev) => !prev);
+    console.log("from card", data);
+    console.log(showModal);
+  };
+
   return (
     <Layout>
       <h1>Paper Cups</h1>
       {data.map((item, index) => (
-        <Card name={item.name} url={item.url} key={index} />
+        <Card
+          name={item.name}
+          url={item.url}
+          key={index}
+          data={showModal}
+          openModal={(e) => {
+            openModal(e);
+          }}
+        />
       ))}
+      <Modal showModal={showModal} setShowModal={setShowModal} />
     </Layout>
   );
 }
