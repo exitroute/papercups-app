fdd27a877e8d8d95621d5b9c94a6e560eae4a6db
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-01-25 00:54:44 +0100

    make container cover screen

--- components/Modal.js
+++ components/Modal.js
@@ -3,16 +3,22 @@ import styled from "styled-components";
 
 const Container = styled.div`
   background: rgba(0, 0, 0, 0.8);
-  position: relative;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 100;
 `;
 
 const Imposter = styled.div`
-  position: absolute;
+  position: relative;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
   max-width: calc(100% - 2rem);
   max-height: calc(100% - 2rem);
+  z-index: 101;
 `;
 
 const Modal = ({ showModal }) => {
