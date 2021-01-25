63da30b46441ade5e071e44c0d5873e03effb534
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-01-25 16:33:17 +0100

    add container component for wizard and plan

---
+++ components/PurchaseWizard.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+export default function PurchaseWizard() {
+  /*
+   * Hooks for:
+   *  setStep
+   *  setUserDetails
+   *  setPaymentDetail
+   *  setConfirmPurchase
+   *  setPurchaseSuccess from server
+   */
+
+  /*
+   * Functions for:
+   *  next
+   *  back
+   *  cancel
+   *  handleChange
+   *  submit
+   *  return to cup page
+   */
+
+  /*
+   * use switch to return components
+   *  case 1 user details
+   *  case 2 payment details
+   *  case 3 review and submit
+   *  case 4 purchase success
+   *
+   */
+
+  return (
+    <div>
+      <p>Buy!</p>
+    </div>
+  );
+}
