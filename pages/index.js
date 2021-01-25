0e28e677c10b344b10f95f8e448d5d959982302c
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-01-25 01:00:06 +0100

    get item data from selected picture & send 2 modal

--- pages/index.js
+++ pages/index.js
@@ -5,26 +5,38 @@ import React, { useState } from "react";
 
 function Index({ data }) {
   const [showModal, setShowModal] = useState(false);
+  const [itemData, setItemData] = useState();
 
   const openModal = (data) => {
     setShowModal((prev) => !prev);
   };
 
+  const getItemData = (data) => {
+    console.log(data);
+    setItemData(data);
+  };
+
   return (
     <Layout>
       <h1>Paper Cups</h1>
+      <Modal
+        showModal={showModal}
+        setShowModal={setShowModal}
+        itemData={itemData}
+      />
       {data.map((item, index) => (
         <Card
           name={item.name}
+          price={item.price}
           url={item.url}
           key={index}
           data={showModal}
           openModal={(e) => {
             openModal(e);
           }}
+          getItemData={(data) => getItemData(data)}
         />
       ))}
-      <Modal showModal={showModal} setShowModal={setShowModal} />
     </Layout>
   );
 }
