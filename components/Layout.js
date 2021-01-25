2fbba40526b17ee19f56f375a6a87a7e391c224d
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-01-25 21:22:59 +0100

    make component generic

--- components/Layout.js
+++ components/Layout.js
@@ -1,16 +1,11 @@
 import { Box, Stack, Center } from "./EveryLayout";
 
-import Nav from "./Nav";
-
 const Layout = (props) => (
-  <>
-    <Nav />
-    <Box>
-      <Center>
-        <Stack>{props.children}</Stack>
-      </Center>
-    </Box>
-  </>
+  <Box>
+    <Center>
+      <Stack>{props.children}</Stack>
+    </Center>
+  </Box>
 );
 
 export default Layout;
