4862e072e5ba50e9e9fc42befe3682ce56232203
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-01-26 22:15:16 +0100

    deploy bugfix

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ module.exports = (phase, { defaultConfig }) => {
   return {
     /* config options for all phases except development here */
     env: {
-      API_URL: "https://papercups-app.vercel.app/api/index",
+      API_URL: "https://papercups-app-exitroute.vercel.app/api/index",
     },
   };
 };
