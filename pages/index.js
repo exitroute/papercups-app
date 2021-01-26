6098c6400fc80dc97f6d7d6ffc8c082dfc8fd7d9
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-01-26 19:40:56 +0100

    add grid to cups page

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Nav from "../components/Nav";
 import Layout from "../components/Layout";
 import Card from "../components/Card";
 import Modal from "../components/Modal";
+import { Box, Center, Grid } from "../components/EveryLayout";
 
 function Index({ data }) {
   const [showModal, setShowModal] = useState(false);
@@ -20,29 +21,33 @@ function Index({ data }) {
   return (
     <>
       <Nav />
-      <Layout>
-        <h1>Paper Cups</h1>
-        <Modal
-          showModal={showModal}
-          openModal={(e) => {
-            openModal(e);
-          }}
-          itemData={itemData}
-        />
-        {data.map((item, index) => (
-          <Card
-            name={item.name}
-            price={item.price}
-            url={item.url}
-            key={index}
-            data={showModal}
-            openModal={(e) => {
-              openModal(e);
-            }}
-            getItemData={(data) => getItemData(data)}
-          />
-        ))}
-      </Layout>
+      <Modal
+        showModal={showModal}
+        openModal={(e) => {
+          openModal(e);
+        }}
+        itemData={itemData}
+      />
+      <Box>
+        <Center measure={"80%"}>
+          <h1>Paper Cups</h1>
+          <Grid>
+            {data.map((item, index) => (
+              <Card
+                name={item.name}
+                price={item.price}
+                url={item.url}
+                key={index}
+                data={showModal}
+                openModal={(e) => {
+                  openModal(e);
+                }}
+                getItemData={(data) => getItemData(data)}
+              />
+            ))}
+          </Grid>
+        </Center>
+      </Box>
     </>
   );
 }
