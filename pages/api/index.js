93b5c87c4f26de3ea6a328d5462383b3f9135dd7
Ryan O'Shea <ryan.oshea@exitroute.org>
2021-01-31 11:23:20 +0100

    logic moved to pages/index

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,41 +1,9 @@
-const faker = require("faker");
-const data = require("./response.json");
-
-const makePicArray = (data) => {
-  const urls = data.results.map((result) => result.original_file_url);
-
-  const titleCapitalized = (fakerWords) => {
-    const words = fakerWords.split(" ");
-    return words
-      .map((word) => {
-        return word[0].toUpperCase() + word.substring(1);
-      })
-      .join(" ");
-  };
-
-  return urls.map((url) => {
-    const title = faker.random.words(2);
-    return {
-      url: url,
-      name: `${titleCapitalized(title)} Cup`,
-      price: `€${faker.random.number({
-        min: 25,
-        max: 50,
-        precision: 5.0,
-      })}.00`,
-    };
-  });
-};
-
-const result = { pictures: makePicArray(data) };
-
 export default (req, res) => {
   try {
     res.statusCode = 200;
     res.setHeader("Content-Type", "application/json");
     res.end(JSON.stringify(result));
   } catch (err) {
-    console.log(err);
     res.send({ error: err.message });
   }
 };
